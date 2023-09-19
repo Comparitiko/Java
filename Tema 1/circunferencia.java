48f4f59c497285dc4b0ac3ea1df44e4c9d58885a
Gabriel Collado <gabigcy@gmail.com>
2023-09-19 23:41:35 +0200

    Añadido calculo circunferencia

---
+++ Tema 1/circunferencia.java
@@ -0,0 +1,21 @@
+// Calcular longitud y area dado el radio de la circunferencia
+class circunferencia {
+  public static void main(String[] args) {
+    // Declarar variables y asignar un numero a radio
+    double radio, longitud, area;
+
+    radio = 5;
+
+    // Calcular area circunferencia
+
+    area = Math.PI * Math.pow(radio, 2);
+
+    // Calcular longitud circunferencia
+ 
+    longitud = Math.PI * 2 * radio;
+
+    // Imprimir resultados
+    System.out.println("La longitud de la circunferencia es: " + longitud);
+    System.out.println("El area de la circunferencia es: " + area);
+  }
+}
