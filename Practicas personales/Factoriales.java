babd7232964079e493f3cacd06a5610e9526012a
Gabriel Collado <gabigcy@gmail.com>
2023-09-22 01:35:31 +0200

    Fix de errores en factoriales

--- Practicas personales/Factoriales.java
+++ Practicas personales/Factoriales.java
@@ -4,14 +4,14 @@ class Factoriales {
     // Declaracion de variable numero y darle el numero 1
     int numero = 1;
     int resultado = 1;
-
+    int repeticiones = 8;
     // Bucle para sacar el factorial de 7
-    while (numero <= 8) {
+    while (numero <= repeticiones) {
       resultado = numero * resultado;
       numero ++;
     }
     
     // Imprimir por consola el resultado
-    System.out.println(numero + "! = " + resultado);
+    System.out.println(repeticiones + "! = " + resultado);
   }
 }
