7c8199249263ee4549a5d72b9c59d9706c62f622
Gabriel Collado <gabigcy@gmail.com>
2023-09-26 22:56:51 +0200

    Ejercicios Introduccion a la programacion finalizados

---
+++ Tema1/Ejercicio18.java
@@ -0,0 +1,32 @@
+/*
+  18. ¿Qué resultados se obtienen al realizar las operaciones siguientes? Si hay errores en la compilación,
+  corríjalos y dé una explicación de por qué suceden.
+
+  int a = 10, b = 3 , e = 1, d, e;
+  float x, y;
+  x= a / b;
+  c = a < b && c;
+  d = a + b++;
+  e = ++a – b;
+  y = (float)a / b;
+
+  Se declaran dos variables llamadas e
+  La variable c no ha sido declarada ni tiene un valor
+  Al no tener valor no puede ser comparado
+ */
+
+public class Ejercicio18 {
+
+  public static void main(String[] args) {
+    
+    int a = 10, b = 3 , e = 1, d;
+    boolean c = true;
+    float x, y;
+    x = a / b;
+    c = a < b && c;
+    d = a + b++;
+    e = ++a - b;
+    y = (float)a / b;
+
+  }
+}
---
+++ Tema1/Ejercicio19.java
@@ -0,0 +1,17 @@
+/*
+  19. Escriba el valor ASCII de la 'J' y de la 'j' sin consultar la tabla.
+ */
+
+public class Ejercicio19 {
+  
+  public static void main(String[] args) {
+    // Declaracion de variables tipo caracter para poder sacar el codigo ascii
+    char J = 'J', j = 'j';
+    
+    // Imprimir por pantalla el codigo
+    System.out.println((int) J);
+    System.out.println((int) j);
+
+  }
+
+}
