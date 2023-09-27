b6fc457dee1a5dcac979cbb04650d059e4f61aab
Gabriel Collado <gabigcy@gmail.com>
2023-09-27 23:09:04 +0200

    Comprimida practica 1

--- Tema1/Practicas-de-introduccion-a-la-programacion/Ejercicio1.java
+++
@@ -1,8 +0,0 @@
-/*
- 1. Si A = 6, B = 2, C = 3, indicar el resultado final de las siguientes expresiones aritméticas:
-    a. (A * C) % C => resultado = 0
-    b. A * B / C => resultado = 4
-    c. C % B + C * B => resultado = 7
-    d. A % ( A * B * C / (B + C) ) => resultado = 7.2
-    e. B * B + C – B * (A % B) => resultado = 7
- */
