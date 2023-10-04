77c645ef85b5ab9e34f7591587064f39036aba30
Gabriel Collado <gabigcy@gmail.com>
2023-10-05 01:03:57 +0200

    Haciendo ejercicio 40

---
+++ Tema1/Ejercicios/T1Ejercicio40.java
@@ -0,0 +1,22 @@
+/*
+  Ejercicio 6
+  Escribe un método que permita cifrar/descifrar una cadena usando el cifrado Cesar, es decir,
+  a cada carácter de la cadena se le suma una cifra en el abecedario. Se pide por teclado la
+  palabra a cifrar/descifrar, la clave (cifra) y lo que se desea hacer, cifrar o descifrar.
+*/
+
+import java.util.Scanner;
+
+public class T1Ejercicio40 {
+  
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    
+
+    sc.close();
+
+  }
+
+}
