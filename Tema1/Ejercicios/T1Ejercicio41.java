61eb9719113cae711cd7d2c42892780bfeca09c1
Comparitiko <gabigcy@gmail.com>
2023-10-05 13:35:06 +0200

    Haciendo Ejercicio 41

---
+++ Tema1/Ejercicios/T1Ejercicio41.java
@@ -0,0 +1,28 @@
+/*
+  Ejercicio 7
+  Escribe un programa que dada una cadena de texto (hola45mundo44) muestre el resultado
+  de sumar todas las cifras numéricas independientes que aparezcan en la cadena.
+*/
+
+import java.util.Scanner;
+
+public class T1Ejercicio41 {
+  
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    // Declaracion de variables
+    
+
+    // Pedir valores por terminal
+    System.out.println("Ingrese una cadena con cifras numericas:");
+    String[] carCad = sc.nextLine().split("");
+
+
+
+    sc.close();
+
+  }
+
+}
