08c207c4b2f2c20808891701d74f5b0b002ebbc0
Gabriel Collado <31490666+Comparitiko@users.noreply.github.com>
2023-10-06 17:44:18 +0200

    Ejemplo Tema 2

---
+++ T2Decenas.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class T2Decenas {
+  
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    for (int i = 100; i <= 300; i += 10) {
+      System.out.println(i);
+    }
+
+    sc.close();
+
+  }
+
+}
---
+++ T2MultiplosCinco.java
@@ -0,0 +1,11 @@
+public class T2MultiplosCinco {
+  
+  public static void main(String[] args) {
+
+    for (int i = 500; i >= 100; i -= 5 ) {
+      System.out.println(i);
+    }
+
+  }
+
+}
