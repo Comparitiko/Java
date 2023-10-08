b635d94129c16b36e74655e7666fc1fb4b23e0aa
Comparitiko <gabigcy@gmail.com>
2023-10-08 16:48:46 +0200

    Ejercicio 52 completado

--- Tema1/Ejercicios/T1Ejercicio52.java
+++ Tema1/Ejercicios/T1Ejercicio52.java
@@ -4,3 +4,36 @@
   números de la sucesión quieres mostrar
 */
 
+import java.util.Scanner;
+
+public class T1Ejercicio52 {
+  
+  public static void sucesionFibonacci (int rep) {
+
+    long[] fibonacci = new long[rep];
+
+    for (int i = 0; i != rep; i++) {
+
+      if (i < 2) {
+        fibonacci[i] = 1;
+      } else {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+      }
+      
+      System.out.println("El numero " + (i + 1) + " de la sucesion de Fibonacci es: " + (fibonacci[i]));
+      
+    }
+  }
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Ingrese la cantidad de numeros quieres que saque de la sucesion de fibonacci");
+    sucesionFibonacci(sc.nextInt());
+
+    sc.close();
+
+  }
+
+}
