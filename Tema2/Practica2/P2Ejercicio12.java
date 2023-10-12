2700b87aaaabf1725958ce4cd23c2659d63f831e
Comparitiko <gabigcy@gmail.com>
2023-10-12 18:52:28 +0200

    Haciendo ejercicio 12

---
+++ Tema2/Practica2/P2Ejercicio12.java
@@ -0,0 +1,32 @@
+/*
+  12. Programa (utilizando bucles) que muestre por pantalla la siguiente figura. Debe funcionar para n =
+  número de líneas, que se pida por teclado.
+     *
+    ***
+   *****
+  *******
+*/
+
+import java.util.Scanner;
+
+public class P2Ejercicio12 {
+  
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    // Pedir numero de filas a pintar
+    System.out.println("Ingrese el numero de filas que quieres pintar:");
+    int filasTotales = sc.nextInt();
+
+    for (int i = 1; i <= filasTotales; i++) {
+      
+      
+
+    }
+
+    sc.close();
+
+  }
+
+}
