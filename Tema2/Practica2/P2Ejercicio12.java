5debe68f5aae00dd85884c7b1abaef85821f8f58
Comparitiko <gabigcy@gmail.com>
2023-10-13 16:35:39 +0200

    Ejercicio 12 completado

--- Tema2/Practica2/P2Ejercicio12.java
+++ Tema2/Practica2/P2Ejercicio12.java
@@ -1,8 +1,8 @@
 /*
   12. Programa (utilizando bucles) que muestre por pantalla la siguiente figura. Debe funcionar para n =
   número de líneas, que se pida por teclado.
-     *
-    ***
+     *  
+    *** 
    *****
   *******
 */
@@ -19,9 +19,29 @@ public class P2Ejercicio12 {
     System.out.println("Ingrese el numero de filas que quieres pintar:");
     int filasTotales = sc.nextInt();
 
+    // Sacar espacios y asteriscos necesarios para la primera fila
+    int espacios = (filasTotales - 1) * 2;
+    int asteriscos = 1;
+
+    // Bucle para poner la fila
     for (int i = 1; i <= filasTotales; i++) {
       
-      
+      // Bucle para poner espacios y asteriscos
+      for (int j = 1; j <= espacios + asteriscos; j++) {
+
+        if (j <= espacios / 2) {
+          System.out.print(" ");
+        } else if (j <= espacios / 2 + asteriscos) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+
+      }
+
+      System.out.println();
+      espacios -= 2;
+      asteriscos += 2;
 
     }
 
