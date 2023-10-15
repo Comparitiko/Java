dc320a34a87d0bac209f218273d466b0db96d01d
Comparitiko <gabigcy@gmail.com>
2023-10-15 19:25:00 +0200

    Haciendo ejercicio 26

--- Tema2/Practica2/P2Ejercicio26.java
+++ Tema2/Practica2/P2Ejercicio26.java
@@ -3,4 +3,21 @@
   tener más de 8 caracteres, al menos una mayúscula y al menos un símbolo (#$%&/()=?¿!
 */
 
+public class P2Ejercicio26 {
 
+  public static String generarPassword () {
+    String[] minus = "abcdefghijklmnopqrstuvwxyz".split("");
+    String[] mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    String[] simbolos = "(#$%&/()=?¿!".split("");
+
+
+    return "Hola";
+  }
+  
+  public static void main(String[] args) {
+
+    System.out.println(generarPassword());
+
+  }
+
+}
