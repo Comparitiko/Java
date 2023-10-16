d5fd1beb0664e492678724d8c9572c27750b2d4d
Comparitiko <gabigcy@gmail.com>
2023-10-16 14:32:55 +0200

    P2 Completada

--- Tema2/Practica2/P2Ejercicio24.java
+++ Tema2/Practica2/P2Ejercicio24.java
@@ -6,4 +6,72 @@
   de intentos probados hasta encontrar el número.
 */
 
+import java.util.Scanner;
 
+public class P2Ejercicio24 {
+
+  // Funcion para generar numero
+  public static int generarNumero (int menor, int mayor) {
+
+    int numRan = (int) (Math.random() * (mayor + 1 - menor)) + menor;
+
+    return numRan;
+
+  }
+  
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    // Declaracion de variables
+    int menor = 1, mayor = 100, num = generarNumero(menor, mayor), opcionMenu = 0, intentos = 0;
+
+    System.out.println("Piensa un numero del 1 al 100");
+
+    // Bucle que pide la opcion del menu y segun lo que se ingrese aumentara el menor o reducira el mayor y si se acierta dira el numero de intentos
+    do {
+
+      try {
+        System.out.println("¿El numero que has pensado es el numero " + num + "?");
+        System.out.println("------------ Menú ------------");
+        System.out.println("1. El numero " + num + " es menor que el numero pensado");
+        System.out.println("2. El numero " + num + " es mayor que el numero pensado");
+        System.out.println("3. El numero " + num + " es igual que el numero pensado");
+        opcionMenu = sc.nextInt();
+
+        if (opcionMenu < 1 || opcionMenu > 3) throw new IllegalArgumentException("Ingrese una opcion valida");
+
+        if (opcionMenu == 1) {
+
+          menor = num + 1;
+          num = generarNumero(menor, mayor);
+          intentos++;
+
+        } else if (opcionMenu == 2) {
+
+          mayor = num - 1;
+          num = generarNumero(menor, mayor);
+          intentos++;
+
+        
+        } else if (menor == mayor) {
+
+          System.out.println("El numero que has pensado es el numero " + num + " y lo he acertado en " + intentos + " intentos");
+
+        } else {
+
+          System.out.println("El numero que has pensado es el numero " + num + " y lo he acertado en " + intentos + " intentos");
+          
+        }
+
+      } catch (IllegalArgumentException err) {
+        System.out.println(err.getMessage());
+      }
+
+    } while (opcionMenu != 3);
+
+    sc.close();
+
+  }
+
+}
--- Tema2/Practica2/P2Ejercicio26.java
+++ Tema2/Practica2/P2Ejercicio26.java
@@ -3,20 +3,80 @@
   tener más de 8 caracteres, al menos una mayúscula y al menos un símbolo (#$%&/()=?¿!
 */
 
+import java.util.Scanner;
+
 public class P2Ejercicio26 {
 
-  public static String generarPassword () {
+  // Funcion para generar numero del 0 a la longitud del array - 1
+  public static int generarNumero (int lenght) {
+
+    int numRan = (int) (Math.random() * (lenght + 1 - 0)) + 0;
+
+    return numRan;
+
+  }
+
+  // Funcion que genera el password y lo controla si esta bien construido o no
+  public static String generarPassword (int caracteres) {
     String[] minus = "abcdefghijklmnopqrstuvwxyz".split("");
     String[] mayus = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     String[] simbolos = "(#$%&/()=?¿!".split("");
+    String[][] tiposCaracteres = {minus, mayus, simbolos};
+    boolean ok = false;
+    int numMayus = 0, numSimbolos = 0;
+    StringBuilder password = new StringBuilder();
 
+    while (!ok) {
 
-    return "Hola";
+      // Generar un caracter
+      int numTiposCaracteres = generarNumero(tiposCaracteres.length - 1);
+      password.append(tiposCaracteres[numTiposCaracteres][generarNumero(tiposCaracteres[numTiposCaracteres].length - 1)]);
+
+      if (numTiposCaracteres == 1) numMayus++;
+      if (numTiposCaracteres == 2) numSimbolos++;
+      if (password.length() == caracteres) {
+
+        if (numMayus >= 1 && numSimbolos >= 1) {
+
+          ok = true;
+
+        } else {
+
+          // Borra el contenido del array y la empieza de nuevo
+          password.setLength(0);
+
+        }
+
+      }
+
+    }
+
+    return password.toString();
+
   }
+
+
   
   public static void main(String[] args) {
 
-    System.out.println(generarPassword());
+    Scanner sc = new Scanner(System.in);
+
+    // Try-catch para capturar si el numero introducido es menor que 8
+    try {
+
+      System.out.println("De cuantos caracteres quieres la contraseña (tiene que ser mayor de 8):");
+      int numCaracteres = sc.nextInt();
+      sc.close();
+
+      if (numCaracteres < 8) throw new IllegalArgumentException("Los caracteres minimo tienen que ser 8");
+
+      System.out.println("La password generada es: " + generarPassword(numCaracteres));
+
+    } catch (IllegalArgumentException err) {
+
+      System.out.println(err.getMessage());
+
+    }
 
   }
 
