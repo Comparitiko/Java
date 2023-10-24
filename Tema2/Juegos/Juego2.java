70a5d2a6133b443c8f2362dc1956c60858ba96b6
Comparitiko <gabigcy@gmail.com>
2023-10-24 17:07:47 +0200

    prueba

--- Tema2/Juegos/Juego2.java
+++ Tema2/Juegos/Juego2.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Juego2 {
   
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    
+
+    sc.close();
+
+  }
+
 }
