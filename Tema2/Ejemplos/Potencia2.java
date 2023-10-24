2fdd10904449034646a3804e066b22c3778ac20b
Comparitikocd <gabigcy@gmail.com>
2023-10-24 20:05:00 +0200

    Potencia con metodo hecha

---
+++ Tema2/Ejemplos/Potencia2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Potencia2 {
+  
+  /**
+   * Funcion que devuelva la potencia dada la base y el exponente
+   * @param base
+   * @param exponente
+   * @return base elevado a exponente
+   */
+  public static double potencia (double base, double exponente) {
+
+    double res = 1;
+    
+    for (int i = 0; i < exponente; i++) {
+
+      res *= base;
+
+    }
+
+    return res;
+
+  }
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    try {
+      System.out.println("Ingrese la base de la potencia:");
+      double base = Double.parseDouble(sc.nextLine());
+
+      System.out.println("Ingrese el exponente de la potencia:");
+      double exponente = Double.parseDouble(sc.nextLine()); 
+
+      // Llamar a la funcion potencia para que la calcule
+      double res = potencia(base, exponente);
+
+      System.out.println("El resultado es: " + res);
+
+    } catch (Exception e) {
+      System.out.println("Error: " + e);
+    } finally {
+      sc.close();
+    }
+
+  }
+
+}
