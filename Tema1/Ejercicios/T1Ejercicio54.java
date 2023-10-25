185a384a71e3405462674f5ca76ed7d391d6fb62
Comparitiko <gabigcy@gmail.com>
2023-10-25 13:49:25 +0200

    Ejercicio 54 completado

--- Tema1/Ejercicios/T1Ejercicio54.java
+++ Tema1/Ejercicios/T1Ejercicio54.java
@@ -27,3 +27,162 @@
   Mostrar pila
   Salir
 */
+
+import java.util.Arrays;
+
+public class T1Ejercicio54 {
+
+  // Funcion que inicializa la pila con *
+  public static String[] inicializarPila (String[] array) {
+
+    for (int i = 0; i < array.length; i++) {
+
+      array[i] = "*";
+
+    }
+
+    return array;
+
+  }
+
+  // Funcion que muestra la longitud de la pila
+  public static int longitudPila (String[] array) {
+
+    int elementos = 0;
+
+    for (int i = 0; i < array.length; i++) {
+
+      if (array[i] != "*") elementos++;
+
+    }
+
+    return elementos;
+
+  }
+
+  // Funcion que devuelve true si esta vacia y false si no
+  public static boolean estaVaciaPila (String[] array) {
+
+    int elementos = longitudPila(array);
+
+    if (elementos != 0) return false;
+    else return true;
+
+  }
+
+  // Funciona que devuelve true si la pila esta lleno y false si no
+  public static boolean estaLlenaPila (String[] array) {
+
+    int elementos = longitudPila(array);
+
+    if (elementos != 10) return false;
+    else return true;
+
+  }
+
+  // Funcion que añade a la pila en la primera posicion el valor del string pasado por parametros
+  public static String[] addPila (String[] array, String cadCar) throws Exception {
+    
+    if (estaLlenaPila(array)) throw new Exception("La pila esta llena");
+
+    for (int i = array.length - 1; i > 0; i--) {
+
+      array[i] = array[i - 1];
+
+    }
+
+    array[0] = cadCar;
+
+    return array;
+
+  }
+
+  // Sacar el ultimo valor añadido de la pila y devolverlo, ademas se pone un asterisco para mostrar que ese valor esta vacio
+  public static String sacarDeLaPila (String[] array) throws Exception {
+
+    if (estaVaciaPila(array)) throw new Exception("La pila esta vacia");
+
+    String elOut = array[0];
+
+    array[0] = "*";
+
+    return elOut;
+
+  }
+
+  // Funcion que muestra todos los elementos de la pila
+  public static void escribirPila (String[] array) {
+
+    System.out.println("Los elementos de la pila son:");
+    System.out.println(Arrays.toString(array));
+
+  }
+  
+  public static void main(String[] args) {
+    
+    // Declaracion de variables
+    int opcionMenu = 0;
+    String cadCar = "", elOut;
+    String[] vector = new String[10];
+    vector = inicializarPila(vector);
+
+    // Menu que pida un numero
+    do {
+
+      System.out.println("---- Menú ----");
+      System.out.println("1. Añadir elemento a la pila");
+      System.out.println("2. Sacar elemento de la pila");
+      System.out.println("3. Longitud de la pila");
+      System.out.println("4. Mostrar pila");
+      System.out.println("5. Salir");
+      try {
+        
+        opcionMenu = Integer.parseInt(System.console().readLine());
+
+        switch (opcionMenu) {
+        case 1:
+          System.out.println("Opcion de añadir elemento a la pila seleccionada...");
+
+          System.out.println();
+
+          System.out.println("Ingrese el elemento que quiere añadir a la pila:");
+          cadCar = System.console().readLine();
+
+          addPila(vector, cadCar);
+          break;
+        case 2:
+          System.out.println("Opcion de sacar elemento de la pila seleccionada...");
+          elOut = sacarDeLaPila(vector);
+
+          System.out.println();
+
+          System.out.println("El elemento extraido de la pila es: " + elOut);
+          break;
+        case 3:
+          System.out.println("Opcion de mostrar longitud de la pila seleccionada...");
+          longitudPila(vector);
+          break;
+        case 4:
+          System.out.println("Opcion de mostrar elementos de la pila seleccionada...");
+          escribirPila(vector);
+          break;
+        case 5:
+          System.out.println("Saliendo...");
+          break;
+    
+        default:
+          System.out.println("Numero incorrecto por favor ingrese uno de los valores mostrados");
+         
+    }
+
+      } catch (Exception err) {
+        
+        System.out.println("Error: " + err.getMessage());
+
+      }
+
+    } while (opcionMenu != 5);
+
+  }
+
+}
