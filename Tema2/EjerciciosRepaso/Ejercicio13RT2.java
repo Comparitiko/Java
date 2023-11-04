0dcb67a0edf16494143069dc2162f132a0e0caa2
Gabriel Collado <gabigcy@gmail.com>
2023-11-04 02:05:18 +0100

    Ejercicio 13 repaso completado

--- Tema2/EjerciciosRepaso/Ejercicio13RT2.java
+++ Tema2/EjerciciosRepaso/Ejercicio13RT2.java
@@ -12,9 +12,36 @@ public class Ejercicio13RT2 {
   public static void main(String[] args) {
     
     try {
-
-      System.out.println("Ingrese el numero de filas a mostrar");
+      // Pedir numero de filas
+      System.out.println("Ingrese el numero de filas a mostrar:");
       int filas = Integer.parseInt(System.console().readLine());
+
+      int espacios = (filas - 1) * 2, nums = 1, num = 1;
+
+      for (int i = 1; i <= filas; i++) {
+      
+        // Bucle para poner espacios y y num dependiendo de la posicion en la que se encuentre el numero
+        for (int j = 1; j <= espacios + nums; j++) {
+  
+          if (j <= espacios / 2) {
+            System.out.print(" ");
+          } else if (j <= espacios / 2 + nums) {
+            if (j > filas) num--;
+            System.out.print(num);
+            if (j < filas) num++;
+          } else {
+            System.out.print(" ");
+          }
+  
+        }
+        // Resetear num para que empiece a contar de nuevo
+        num = 1;
+  
+        System.out.println();
+        espacios -= 2;
+        nums += 2;
+  
+      }
       
       
     } catch (Exception e) {
