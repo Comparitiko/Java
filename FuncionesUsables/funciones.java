cb6aaa3353410d444c3d589f49a13b83aa063d04
Comparitikocd <gabigcy@gmail.com>
2023-11-10 16:27:43 +0100

    Creacion de funciones usables en otros archivos

---
+++ FuncionesUsables/funciones.java
@@ -0,0 +1,18 @@
+package FuncionesUsables;
+
+public class funciones {
+
+  // Funcion que genera un numero del 1 al 6
+  public static int tirarDado6 () {
+    return (int) Math.ceil(Math.random() * 6);
+  } 
+
+  // Generar un numero aleatorio entre un min y un max
+  public static int genNum(int min, int max) {
+
+    return (int) ((Math.random() * (max - min + 2) + min - 1));
+
+  }
+  
+  
+}
---
+++ Tema3/Ejemplos/Arrays/prueba.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import FuncionesUsables.funciones;
+
+public class prueba {
+  
+  public static void main(String[] args) {
+
+    System.out.println(funciones.tirarDado6());
+
+  }
+
+}
