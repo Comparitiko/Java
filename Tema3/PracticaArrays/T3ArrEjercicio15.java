cca725f594741e97c1ebcf25bbf9def414eecc66
Comparitiko <gabigcy@gmail.com>
2023-11-14 15:42:05 +0100

    Ejercicio 15 completado

---
+++ Tema3/PracticaArrays/T3ArrEjercicio15.java
@@ -0,0 +1,54 @@
+/*
+  15. Crear dos matrices 3x3 de números enteros (generadas aleatoriamente), pintarlas, sumarlas y mostrar su suma. El
+  resultado será también una matriz (suma) donde cada elemento será la suma de los elementos de las otras
+  matrices en la misma posición.
+*/
+
+import java.util.Arrays;
+
+public class T3ArrEjercicio15 {
+
+  public static int genNum () {
+    return (int) Math.ceil(Math.random() * 9);
+  }
+  
+  public static void main(String[] args) {
+
+    int sumaNums = 0;
+    int[][] matriz = new int[3][3], suma = new int[3][3];
+    
+    // Insertar numeros aleatorios en el array e ir calculando la suma e insertarla en el segundo array
+    for (int i = 0; i != matriz.length; i++) {
+      
+      for (int j = 0; j != matriz[i].length; j++) {
+        
+        matriz[i][j] = genNum();
+
+        sumaNums += matriz[i][j];
+        suma[i][j] = sumaNums;
+
+      }
+
+    }
+
+    // Mostrar por pantalla el array generado aleatoriamente
+    System.out.println("El array generado aleatoriamente es:");
+
+    for (int i = 0; i < matriz.length; i++) {
+      
+      System.out.println(Arrays.toString(matriz[i]));
+  
+    }
+
+    // Mostrar por pantalla el array suma
+    System.out.println("El array generado a partir de las sumas del primer array es:");
+
+    for (int i = 0; i < matriz.length; i++) {
+      
+      System.out.println(Arrays.toString(suma[i]));
+  
+    }
+  
+  }
+
+}
---
+++ Tema3/PracticaArrays/T3ArrEjercicio16.java
@@ -0,0 +1,13 @@
+/*
+  16. Crea una matriz de 3x6 números enteros aleatorios no repetidos.
+*/
+
+public class T3ArrEjercicio16 {
+  
+  public static void main(String[] args) {
+
+    
+
+  }
+
+}
