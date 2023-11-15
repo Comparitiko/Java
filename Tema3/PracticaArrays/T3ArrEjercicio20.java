10df9693ae392ee5986b054fa77b73cdf5de0a77
Comparitikocd <gabigcy@gmail.com>
2023-11-15 19:01:11 +0100

    Haciendo ejercicio 20 y 21

--- Tema3/PracticaArrays/T3ArrEjercicio20.java
+++ Tema3/PracticaArrays/T3ArrEjercicio20.java
@@ -7,3 +7,37 @@
   El juego debe pedir las posiciones donde el jugador actual quiera poner su marca ( [0,0], [0,1], [0,2], [1,0], [1,1],
   [1,2], [2,0], [2,1], [2,2] ), esta debe ser validada y por supuesto que no haya una marca ya puesta.
 */
+
+import java.util.Arrays;
+
+public class T3ArrEjercicio20 {
+
+  // Funcion que me indica si esta marcada la casilla pasada
+  public static boolean estaMarcada (char[][] array, int i, int j) {
+    
+    if (array[i][j] != 'X' || array[i][j] != 'O') return false;
+    else return true;
+
+  }
+
+  // Funcion que comprueba despues de cada tirada si hay ganador
+  public static boolean hayGanador (char[][] array) {
+    
+    return false;
+
+  }
+
+  
+  
+  public static void main(String[] args) {
+
+    char[][] tablero = new char[3][3];
+
+    for (int i = 0; i < tablero.length; i++) {
+      System.out.println(Arrays.toString(tablero[i]));
+    }
+
+
+  }
+
+}
