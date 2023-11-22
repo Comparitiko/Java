4a02fc93efe4cd97e78ecbc829447a529dd12f8e
Comparitikocd <gabigcy@gmail.com>
2023-11-22 18:53:44 +0100

    Haciendo ejercicio3

--- Tema3/PracticaStrings/T3StrEjercicio3.java
+++ Tema3/PracticaStrings/T3StrEjercicio3.java
@@ -2,3 +2,27 @@
   3. Diseña un programa en Java que solicite al usuario una cadena en la que buscará y otra que será la
   cadena buscada. El programa indicará cuántas veces aparece la segunda cadena en la primera.
 */
+
+public class T3StrEjercicio3 {
+  
+  public static void main(String[] args) {
+
+    try {
+    
+      System.out.println("Ingrese la cadena:");
+      String frase = System.console().readLine();
+
+      System.out.println("Ingrese la cadena a buscar");
+      String fraseABuscar = System.console().readLine();
+
+      int contadorVeces = 0;
+    
+    } catch (Exception e) {
+    
+        System.out.println("Error: " + e);
+    
+    }
+
+  }
+
+}
