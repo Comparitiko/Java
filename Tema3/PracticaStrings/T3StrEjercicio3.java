fde48f82ef7e22f1393196d3166288d9a6debb0b
Comparitiko <gabigcy@gmail.com>
2023-11-22 16:51:09 +0100

    Haciendo ejercicios strings

---
+++ Tema3/PracticaStrings/T3StrEjercicio3.java
@@ -0,0 +1,4 @@
+/*
+  3. Diseña un programa en Java que solicite al usuario una cadena en la que buscará y otra que será la
+  cadena buscada. El programa indicará cuántas veces aparece la segunda cadena en la primera.
+*/
