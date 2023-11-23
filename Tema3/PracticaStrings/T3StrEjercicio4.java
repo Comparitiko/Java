aea173343c83dd9ca8d3eef8d255165d0fadf1df
Comparitiko <gabigcy@gmail.com>
2023-11-23 14:01:48 +0100

    Haciendo ejercicio 4

--- Tema3/PracticaStrings/T3StrEjercicio4.java
+++ Tema3/PracticaStrings/T3StrEjercicio4.java
@@ -1,9 +1,11 @@
 /*
-  3. Diseña un programa en Java que solicite al usuario una cadena en la que buscará y otra que será la
-  cadena buscada. El programa indicará cuántas veces aparece la segunda cadena en la primera.
+  4. Crea un programa en Java que solicite al usuario dos cadenas de caracteres y que devuelva la primera
+  cadena, pero transformando en mayúsculas la parte que coincide con la segunda cadena introducida.
+  Por ejemplo, si se introducen las cadenas “Esta es mi amiga Ana” y “amiga” devolverá “Esta es mi
+  AMIGA Ana”.
 */
 
-public class T3StrEjercicio3 {
+public class T3StrEjercicio4 {
   
   public static void main(String[] args) {
 
