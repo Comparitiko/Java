7ec23ae25dac0b3ff789300c2855a6ab11013c3f
Comparitiko <gabigcy@gmail.com>
2023-11-24 14:10:43 +0100

    Ejercicio 8 completado

--- Tema3/PracticaStrings/T3StrEjercicio6.java
+++ Tema3/PracticaStrings/T3StrEjercicio6.java
@@ -5,12 +5,8 @@
 public class T3StrEjercicio6 {
 
   // Metodo que compara si la cadena ingresada es igual a cadena dada la vuelta
-  public static boolean esPalindromo (StringBuffer cadena) {
-    System.out.println(cadena);
-    System.out.println(cadena.reverse());
-    System.out.println(cadena == cadena.reverse());
-    if (cadena == cadena.reverse()) return true;
-    else return false;
+  public static boolean esPalindromo (StringBuilder cadena) {
+    return (cadena.toString().equals(cadena.reverse().toString()));
   }
   
   public static void main(String[] args) {
@@ -18,7 +14,7 @@ public class T3StrEjercicio6 {
     try {
       
       System.out.println("Ingresa una cadena y te dire si es palindromo:");
-      StringBuffer cad = new StringBuffer(System.console().readLine());
+      StringBuilder cad = new StringBuilder(System.console().readLine());
 
       if (esPalindromo(cad)) System.out.println("La cadena ingresada es un palindromo");
       else System.out.println("La cadena ingresada no es un palindromo");
